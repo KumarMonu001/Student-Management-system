94c885d2f03c879cf89618ea8bd3f32892d80014
Monu <monukumar8317709490@gmail.com>
2025-03-20 16:54:57 +0530

    program basic structure

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+# insert student data into the list
+def insert():
+    global empdb
+    global lst
+    cl = []
+    for i in lst:
+        cl.append(input(f'Enter {i} '))
+    empdb.append(cl)
 
-empbd = []
+def update(studentIndex):
+    pass
+
+def delete(studentIndex):
+    pass
+
+def show_one_student(studentIndex):
+    global lst
+    global empdb
+    for attribute in range(0, 5):
+            print(f"{lst[attribute].capitalize()}\t  : {empdb[studentIndex][attribute].capitalize()}")
+
+
+def show_all_student():
+    pass
+
+def search(rollno) -> int:
+    global empdb
+    index = -1
+    for student in empdb:
+        if student[0] == rollno:
+            index = empdb.index(student)
+            return index
+    return index
+
+# empdb -> contains the data of the each student
+empdb = []
+# lst -> list contains all the essential attributes of the student
+lst = ['name', 'rollno','semester', 'cgpa', 'email']
 
 flag = True
 while flag:
@@ -9,16 +45,14 @@ while flag:
     3 : Delete student details
     4 : Show all students
     5 : Search students
-    5 : Exit
+    6 : Exit
     """
     choice = int(input(menu_data + 'Enter your choice : '))
     
     if choice == 1:
-        cl = []
-        lst = ['rollno', 'name', 'email', 'semester', 'cgpa']
-        for i in lst:
-            cl.append(input(f'Enter {i}'))
-        empbd.append(cl)
+        # Insert parts
+        insert()
+        print(empdb)
     elif choice == 2:
         # Update parts
         pass
@@ -31,8 +65,13 @@ while flag:
     elif choice == 5:
         # searching part here
         pass
-    elif choice == 5:
+    elif choice == 6:
         # for exiting from the loop
-        pass
+        stdRoll = input('Enter student roll no. : ')
+        find = search(stdRoll)
+        if stdRoll != -1:
+            show_one_student(find)
+        else:
+            print(f"Student with roll no. {stdRoll} doesn't exist!")
     else:
         print('Enter a valid choice!')
